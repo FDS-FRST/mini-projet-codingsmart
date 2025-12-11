1f228ce1165bf8b3ad2106e2d3585cb4ecd2225c
Patrick Charles <pcharles@culligan.ht>
2025-12-10 21:54:44 -0500

    Finalisation de Joueur et Personnage avant fusion

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,5 +1,18 @@
-public class joeur {
+public class Joueur {
     private String nom;
     private Personnage[] equipe; // tableau pour 3 personnages
+public String getNom() {
+    return this.nom;
+}
+public void setNom(String nom) {
+    this.nom = nom;
+}
+public Personnage[] getEquipe() {
+    return this.equipe;
+}
+public void setEquipe(Personnage[] equipe) {
+this.equipe = equipe;
+}
+
 
 }
--- src/Personnage.java
+++ src/Personnage.java
@@ -1,10 +1,10 @@
-public class personnage {
+public class Personnage {
 
 private String nom;
 private int vie;
 private int attaquer;
 
-public personnage( String nom, int vie , int attaquer ) {
+public Personnage( String nom, int vie , int attaquer ) {
     this.nom = nom;
     this.vie = vie;
     this.attaquer = attaquer;
