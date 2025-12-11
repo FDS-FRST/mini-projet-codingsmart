a550dfc0a954957cb004edf28d77f3fb41ef9f1c
Patrick Delva <delvapatrick25@gmail.com>
2025-12-10 19:18:32 -0500

    J'ai creer les Attributs des persoonages et ensuite le constructeur

--- src/personnage.java
+++ src/personnage.java
@@ -1,2 +1,12 @@
 public class personnage {
+
+private String nom;
+private int vie;
+private int attaquer;
+
+public personnage( String nom, int vie , int attaquer ) {
+    this.nom = nom;
+    this.vie = vie;
+    this.attaquer = attaquer;
+}
 }
