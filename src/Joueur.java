cbd7d8e870016dde58db062cf56c2dbb64419ad9
Delva Patrick <delvapatrick25@gmail.com>
2025-12-19 18:26:05 -0500

    Creation d'une equipe en initialisant le tableau qui pourra contenir les trois personnages

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,19 +1,35 @@
+import java.util.Scanner;
+
 public class Joueur {
     private String nom;
     private Personnage[] equipe; // tableau pour 3 personnages
 
-public String getNom() {
-    return this.nom;
-}
-public void setNom(String nom) {
-    this.nom = nom;
-}
-public Personnage[] getEquipe() {
-    return this.equipe;
-}
-public void setEquipe(Personnage[] equipe) {
-this.equipe = equipe;
-}
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Personnage[] getEquipe() {
+        return this.equipe;
+    }
+
+    public void setEquipe(Personnage[] equipe) {
+        this.equipe = equipe;
+    }
+
+    public void creerEquipe(Scanner sc) {
+        equipe = new Personnage[3]; // initialisation du tableau
+        System.out.println(nom + ", crée ton équipe :");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Nom du personnage " + (i + 1) + " : ");
+            String pNom = sc.nextLine();
+            equipe[i] = new Personnage(pNom, 100, 20); // vie = 100, attaque = 20
+        }
+    }
 
 
 }
