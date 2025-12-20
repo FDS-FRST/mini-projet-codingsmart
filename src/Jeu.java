4b69be2f6b969b3e75a88e63f7526e6d4f53c214
Delva Patrick <delvapatrick25@gmail.com>
2025-12-20 17:48:13 -0500

    Finalisation de la Classe Jeu et ensuite permet a la classe Main de simplement lancer la classe Jeu avec le Jeu.main(args)

--- src/Jeu.java
+++ src/Jeu.java
@@ -21,5 +21,49 @@ public class Jeu {
         j1.creerEquipe(sc);
         System.out.println();
         j2.creerEquipe(sc);
+
+        int tour = 1;
+
+        // ----- Phase 2 : Boucle de combat -----
+        while (j1.aEncoreDesPersonnagesVivants() && j2.aEncoreDesPersonnagesVivants()) {
+
+            System.out.println("\n--- TOUR " + tour + " ---");
+
+            // Tour du joueur 1
+            System.out.println("\n" + j1.getNom() + " attaque !");
+            j1.afficherEquipe();
+            Personnage attaquant1 = j1.choisirPersonnageVivant(sc, "Choisis ton attaquant (1-3) : ");
+
+            j2.afficherEquipe();
+            Personnage cible1 = j2.choisirPersonnageVivant(sc, "Choisis une cible (1-3) : ");
+
+            attaquant1.attaquer(cible1);
+
+            // Vérification si le joueur 2 a perdu
+            if (!j2.aEncoreDesPersonnagesVivants()) break;
+
+            // Tour du joueur 2
+            System.out.println("\n" + j2.getNom() + " attaque !");
+            j2.afficherEquipe();
+            Personnage attaquant2 = j2.choisirPersonnageVivant(sc, "Choisis ton attaquant (1-3) : ");
+
+            j1.afficherEquipe();
+            Personnage cible2 = j1.choisirPersonnageVivant(sc, "Choisis une cible (1-3) : ");
+
+            attaquant2.attaquer(cible2);
+
+            tour++;
+        }
+
+        // ----- Fin du jeu -----
+        System.out.println("\n===== FIN DU JEU =====");
+
+        if (j1.aEncoreDesPersonnagesVivants()) {
+            System.out.println("Victoire de " + j1.getNom() + " ! Tous les personnages de " + j2.getNom() + " sont éliminés.");
+        } else {
+            System.out.println("Victoire de " + j2.getNom() + " ! Tous les personnages de " + j1.getNom() + " sont éliminés.");
+        }
+
+        sc.close();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
         System.out.println("==== Mini-projet Java : Jeu de personnages (Console) ===");
+        Jeu.main(args);
     }
 
 }
