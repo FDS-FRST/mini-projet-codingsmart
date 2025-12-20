a9aaadbaf371200a40ff7be2037c287dbcc1234f
Delva Patrick <delvapatrick25@gmail.com>
2025-12-20 16:59:34 -0500

    Creations des Joeurs et des Equipes

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Jeu {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bienvenue dans le Jeu de Combat !\n");
+
+        // ----- Phase 1 : Création des joueurs -----
+        System.out.print("Joueur 1, entre ton nom : ");
+        Joueur j1 = new Joueur();
+        j1.setNom(sc.nextLine());
+
+        System.out.print("Joueur 2, entre ton nom : ");
+        Joueur j2 = new Joueur();
+        j2.setNom(sc.nextLine());
+
+        // Création des équipes
+        j1.creerEquipe(sc);
+        System.out.println();
+        j2.creerEquipe(sc);
+    }
 }
